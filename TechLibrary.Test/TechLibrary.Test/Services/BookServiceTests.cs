fabc987f6a92f6ea2375af846119365c8c0eb6e2
Gavin Hill <gchill97@gmail.com>
2021-05-20 21:35:26 -0500

    Add overloaded method GetBooksAsync with pagination capability

---
+++ TechLibrary.Test/TechLibrary.Test/Services/BookServiceTests.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechLibrary.Data;
+using TechLibrary.Domain;
+using TechLibrary.Services;
+
+namespace TechLibrary.Test.Services
+{
+    [TestFixture]
+    public class BookServiceTests
+    {
+        private DataContext _dataContext;
+        private DbConnection _connection;
+        private BookService _bookService;
+
+        [OneTimeSetUp]
+        public void TestSetup()
+        {
+            _connection = CreateInMemoryConnection();
+            _dataContext = new DataContext(new DbContextOptionsBuilder<DataContext>().UseSqlite(_connection).Options);
+            _bookService = new BookService(_dataContext);
+
+            Seed();
+        }
+
+        private DbConnection CreateInMemoryConnection()
+        {
+            var connection = new SqliteConnection("Filename=:memory:");
+
+            connection.Open();
+
+            return connection;
+        }
+
+        private void Seed()
+        {
+            _dataContext.Database.EnsureDeleted();
+            _dataContext.Database.EnsureCreated();
+
+            _dataContext.AddRange(
+                new Book() { BookId = 1 },
+                new Book() { BookId = 2 },
+                new Book() { BookId = 3 },
+                new Book() { BookId = 4 },
+                new Book() { BookId = 5 });
+
+            _dataContext.SaveChanges();
+        }
+
+        [OneTimeTearDown]
+        public void TestTearDown()
+        {
+            _dataContext.Dispose();
+            _connection.Dispose();
+        }
+
+        [Test]
+        public async Task GetBooksAsync_ReturnsAllBooks()
+        {
+            var books = await _bookService.GetBooksAsync();
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(1, books[0].BookId);
+                Assert.AreEqual(2, books[1].BookId);
+                Assert.AreEqual(3, books[2].BookId);
+                Assert.AreEqual(4, books[3].BookId);
+                Assert.AreEqual(5, books[4].BookId);
+            });
+        }
+
+        [Test]
+        public async Task GetBooksAsync_PageSize0_ReturnsEmpty()
+        {
+            var books1 = await _bookService.GetBooksAsync(0, 0);
+            var books2 = await _bookService.GetBooksAsync(1, 0);
+            
+
+            Assert.AreEqual(0, books1.Count);
+            Assert.AreEqual(0, books2.Count);
+        }
+
+        [Test]
+        public async Task GetBooksAsync_Page0_ReturnsFirstPage()
+        {
+            var books1 = await _bookService.GetBooksAsync(0, 1);
+            var books2 = await _bookService.GetBooksAsync(0, 2);
+
+            Assert.AreEqual(1, books1.Count);
+            Assert.AreEqual(1, books1[0].BookId);
+
+            Assert.AreEqual(2, books2.Count);
+            Assert.AreEqual(1, books2[0].BookId);
+            Assert.AreEqual(2, books2[1].BookId);
+        }
+
+        [Test]
+        public async Task GetBooksAsync_Page1AndPageSize1_ReturnsFirstBook()
+        {
+            var books = await _bookService.GetBooksAsync(1, 1);
+
+            Assert.AreEqual(1, books.Count);
+            Assert.AreEqual(1, books[0].BookId);
+        }
+
+        [Test]
+        public async Task GetBooksAsync_Page2AndPageSize1_ReturnsSecondBook()
+        {
+            var books = await _bookService.GetBooksAsync(2, 1);
+
+            Assert.AreEqual(1, books.Count);
+            Assert.AreEqual(2, books[0].BookId);
+        }
+
+        [Test]
+        public async Task GetBooksAsync_Page1AndPageSize2_ReturnsFirstTwoBooks()
+        {
+            var books = await _bookService.GetBooksAsync(1, 2);
+
+            Assert.AreEqual(2, books.Count);
+            Assert.AreEqual(1, books[0].BookId);
+            Assert.AreEqual(2, books[1].BookId);
+        }
+
+        [Test]
+        public async Task GetBooksAsync_Page2AndPageSize2_ReturnsSecondTwoBooks()
+        {
+            var books = await _bookService.GetBooksAsync(2, 2);
+
+            Assert.AreEqual(2, books.Count);
+            Assert.AreEqual(3, books[0].BookId);
+            Assert.AreEqual(4, books[1].BookId);
+        }
+
+        [Test]
+        public async Task GetBooksAsync_Page3AndPageSize2_ReturnsLastBook()
+        {
+            var books = await _bookService.GetBooksAsync(3, 2);
+
+            Assert.AreEqual(1, books.Count);
+            Assert.AreEqual(5, books[0].BookId);
+        }
+
+        [Test]
+        public async Task GetBooksAsync_Page4AndPageSize2_ReturnsEmpty()
+        {
+            var books = await _bookService.GetBooksAsync(4, 2);
+
+            Assert.AreEqual(0, books.Count);
+        }
+    }
+}
--- TechLibrary/Services/BookService.cs
+++ TechLibrary/Services/BookService.cs
@@ -12,6 +12,12 @@ namespace TechLibrary.Services
     public interface IBookService
     {
         Task<List<Book>> GetBooksAsync();
+        /// <summary>
+        /// Get books by page.
+        /// </summary>
+        /// <param name="page">The page number starting at 1.</param>
+        /// <param name="pageSize">The size for each page.</param>
+        Task<List<Book>> GetBooksAsync(int page, int pageSize);
         Task<Book> GetBookByIdAsync(int bookid);
     }
 
@@ -29,6 +35,13 @@ namespace TechLibrary.Services
             var queryable = _dataContext.Books.AsQueryable();
 
             return await queryable.ToListAsync();
+        }
+
+        public async Task<List<Book>> GetBooksAsync(int page, int pageSize)
+        {
+            var queryable = _dataContext.Books.AsQueryable();
+
+            return await queryable.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
         }
 
         public async Task<Book> GetBookByIdAsync(int bookid)
