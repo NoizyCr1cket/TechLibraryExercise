80f713656aa62c94e86590068f5ef88e631486f7
Gavin Hill <gchill97@gmail.com>
2021-05-22 21:06:20 -0500

    Add search box to the UI

--- TechLibrary.Web/src/components/Home.vue
+++ TechLibrary.Web/src/components/Home.vue
@@ -1,9 +1,20 @@
 <template>
     <div class="home">
         <h1>{{ msg }}</h1>
-
-        <b-pagination v-model="currentPage" :total-rows="totalItems" :per-page="perPage" align="center" v-on:change="onPageChanged"></b-pagination>
-        <b-table striped hover :items="dataContext" :fields="fields" responsive="sm" :per-page="10" :current-page="currentPage">
+        <b-row>
+            <b-col sm="6">
+                <b-pagination v-model="currentPage" :total-rows="totalItems" :per-page="perPage" v-on:change="onPageChanged"></b-pagination>
+            </b-col>
+            <b-col sm="6">
+                <b-input-group>
+                    <b-form-input v-model="query" placeholder="Search for a book" @keydown.enter.native="onSearch"></b-form-input>
+                    <b-input-group-append>
+                        <b-button variant="primary" v-on:click="onSearch">Search</b-button>
+                    </b-input-group-append>
+                </b-input-group>
+            </b-col>
+        </b-row>
+        <b-table striped hover :items="dataContext" :fields="fields" responsive="sm" :per-page="10" :current-page="currentPage" :filter="filter">
             <template v-slot:cell(thumbnailUrl)="data">
                 <b-img :src="data.value" thumbnail fluid></b-img>
             </template>
@@ -34,11 +45,13 @@
             currentPage: 1,
             perPage: 10,
             totalItems: 0,
+            query: '',
+            filter: '',
         }),
         
         methods: {
             dataContext(ctx, callback) {
-                axios.get(`https://localhost:5001/books?page=${ctx.currentPage}&pageSize=${ctx.perPage}`)
+                axios.get(`https://localhost:5001/books?page=${ctx.currentPage}&pageSize=${ctx.perPage}&query=${ctx.filter}`)
                     .then(response => {
                         this.totalItems = response.data.totalCount;
                         this.currentPage = response.data.pageNumber;
@@ -47,6 +60,9 @@
             },
             onPageChanged(pageNumber) {
                 this.currentPage = pageNumber;
+            },
+            onSearch() {
+                this.filter = this.query;
             }
         }
     };
@@ -54,5 +70,8 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+    .nav {
+        display: flex;
+    }
 </style>
 
